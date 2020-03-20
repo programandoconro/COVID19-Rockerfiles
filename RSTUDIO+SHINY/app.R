db06f6bee67cd72542c78b07b57e6ae00de8545e
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-03-20 13:25:22 +0000

    Update app.R

--- RSTUDIO+SHINY/app.R
+++ RSTUDIO+SHINY/app.R
@@ -9,7 +9,6 @@ str(covid_19)
 summary(covid_19)
 plot(covid_19[,c('Confirmed','Deaths','Recovered')])
 
-
 Spain <- filter(covid_19, covid_19$Country.Region=='Spain')
 
 topCountries <- filter(covid_19,covid_19$Deaths>500)
@@ -24,21 +23,44 @@ f <- filter(df,df$Deaths>r)
 
 return (
   
-  plot(as.factor(droplevels(f$Country.Region)),f[,var])
+  plot(as.factor(droplevels(f$Country.Region)),f[,var],ylab=var, xlab='Countries')
 
 )
 }
 
-showTopCountries(100, 'Deaths')
+showTopCountries(100, 'Recovered')
 
+ui <- fluidPage(
+  sidebarLayout(
+    
+    sidebarPanel(
+      
+      sliderInput(inputId = "alpha",
+                  label = "Amount of People:",
+                  min = 1,
+                  max = 4000,
+                  value = 50),
+      
+      selectInput("var","Variable",c("Deaths","Recovered","Confirmed")),
+      
+    ),
+    
+    mainPanel(
+      
+      plotOutput(outputId = "boxPlot")
+      
+    )
+  )
+)
 
+server <- function(input, output) {
+  
+  output$boxPlot <- renderPlot({
+    
+    showTopCountries(input$alpha, input$var)
+   
+  })
+  
+}
 
-
-
-
-
-
-
-
-
-
+shinyApp(ui, server)
