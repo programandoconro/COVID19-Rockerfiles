f1e9221d73483a577e881a8848bfcef93fc20827
sony <sony@localhost.localdomain>
2020-03-20 15:05:16 +0000

    clean

--- FLASK/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-	return "Go to app.py and start writing a code"
-
-if __name__ == "__main__":
-	app.run()
--- SHINY/kaggle.R
+++
@@ -1,6 +0,0 @@
-library(reticulate)
-source_python('kaggle.py')
-unzip('novel-corona-virus-2019-dataset.zip')
-
-df_covid = read.csv('covid_19_data.csv')
-
